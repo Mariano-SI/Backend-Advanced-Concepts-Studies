354de3fae16b1db0c3bb06196e6bb24f2b91e4a2
Mariano-SI <Mariano.silva@ufvjm.edu.br>
2024-08-03 17:14:04 -0300

    criando services de produtos

---
+++ src/modules/products/services/UpdateProductService.ts
@@ -0,0 +1,46 @@
+import { getCustomRepository } from "typeorm";
+import { ProductRepository } from "../typeorm/repositories/ProductsRepository";
+import AppError from "../../../shared/errors/AppError";
+import Product from "../typeorm/entities/Product";
+
+interface IRequest{
+    id: string;
+    name: string;
+    price: number;
+    quantity: number;
+}
+
+class UpdateProductService{
+    public async execute({id, name, price, quantity}: IRequest): Promise<Product>{
+        const productsRepository = getCustomRepository(ProductRepository);
+        const product = await productsRepository.findOne(id);
+
+        if(!product){
+            throw new AppError('Product not found.', 404);
+        }
+
+        if(name){
+            const newNameAlreadyExists = await productsRepository.findByName(name);
+
+            if(newNameAlreadyExists){
+                throw new AppError('There is already one product with this name', 409);
+            }
+
+            product.name = name;
+        }
+
+        if(price !== undefined){
+            product.price = price;
+        }
+
+        if(quantity !== undefined){
+            product.quantity = quantity;
+        }
+
+        await productsRepository.save(product);
+
+        return product;
+    }
+}
+
+export default UpdateProductService;

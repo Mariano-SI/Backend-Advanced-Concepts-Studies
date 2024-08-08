82d181becf30b4921fbd79c734fb372fb314f58c
Mariano-SI <Mariano.silva@ufvjm.edu.br>
2024-08-07 22:33:46 -0300

    sobreescrevendo definicao do Request do express e adicionando id do usuário no middleware

--- src/modules/users/controller/UserController.ts
+++ src/modules/users/controller/UserController.ts
@@ -4,6 +4,7 @@ import CreateUserService from "../services/CreateUserService";
 
 export default class UserController {
   public async index(request: Request, response: Response): Promise<Response> {
+
     const listUsers = new ListUsersServices();
     const users = await listUsers.execute();
     return response.json(users);
--- src/shared/http/middlewares/isAuthenticated.ts
+++ src/shared/http/middlewares/isAuthenticated.ts
@@ -1,6 +1,13 @@
 import { Request, Response, NextFunction } from 'express';
 import { verify } from 'jsonwebtoken';
-import AppError from '../../../shared/errors/AppError';
+import AppError from '../../errors/AppError';
+
+interface ITokenPayload{
+    iat: number;
+    exp: number;
+    sub: string;
+}
+
 export default function isAuthenticated(request: Request, response: Response, next: NextFunction):void{
     const authHeader = request.headers.authorization;
 
@@ -12,6 +19,13 @@ export default function isAuthenticated(request: Request, response: Response, ne
 
     try {
         const decodeToken = verify(token, process.env.JWT_SECRET);
+        
+        const { sub } = decodeToken as ITokenPayload;
+
+        request.user = {
+            id: sub
+        };
+
         return next();
     } catch (error) {
         throw new AppError('Invalid JWT token.', 401);

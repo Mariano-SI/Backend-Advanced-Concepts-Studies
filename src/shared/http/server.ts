5c2e1b375ca19b8afe068234f3b51356b39fa99b
Mariano-SI <Mariano.silva@ufvjm.edu.br>
2024-08-07 23:51:03 -0300

    rota estatica para acesar arquivos upados

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -8,6 +8,7 @@ import cors from 'cors';
 import routes from './routes';
 import AppError from '../errors/AppError';
 import '../typeorm';
+import uploadConfig from "../../config/upload"
 
 config();
 
@@ -15,6 +16,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/files', express.static(uploadConfig.directory));
 
 app.use(routes);
 app.use(errors());

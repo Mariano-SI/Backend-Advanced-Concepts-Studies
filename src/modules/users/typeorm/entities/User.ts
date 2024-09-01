079215878426b6bd83706eaab1859681f6dbaf6b
Mariano-SI <Mariano.silva@ufvjm.edu.br>
2024-09-01 19:13:50 -0300

    feat: corrigindo entidade user

--- src/modules/users/typeorm/entities/User.ts
+++ src/modules/users/typeorm/entities/User.ts
@@ -26,14 +26,7 @@ export class User {
     @UpdateDateColumn()
     updated_at: Date
 
-    @Expose({name: 'avatar_url'})
-    getAvatarUrl(): string | null{
-        if(!this.avatar){
-            return null
-        }
 
-        return `${process.env.APP_API_URL}/files/${this.avatar}`
-    }
     @Expose({name: 'avatar_url'})
     getAvatarUrl(): string | null { 
         if(!this.avatar){ 
